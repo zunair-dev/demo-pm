8c97f56d708104e2813fe821decddaf21f5e6d90
Muhammad Zunair <muhammad.zunair@xprolabs.com>
2021-08-31 14:02:57 +0500

    dependent destroy on projects & tasks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  
+
   def after_sign_in_path_for(resource)
     if current_user.admin == true
       stored_location_for(resource) || :root
     else
-      stored_location_for(resource) || :root
+      stored_location_for(resource) || :root # pending
     end
   end
 
