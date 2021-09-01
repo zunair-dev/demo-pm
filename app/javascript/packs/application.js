b9d5180e24b03dc57f863236b6917a3c501b3175
Muhammad Zunair <muhammad.zunair@xprolabs.com>
2021-09-01 15:30:24 +0500

    configure bootstrap via yarn, webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import 'bootstrap'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("packs/custom")
+require("packs/posts.js")
 
 Rails.start()
 Turbolinks.start()
