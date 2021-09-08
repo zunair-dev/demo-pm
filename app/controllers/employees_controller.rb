3859d119dc02a27e76c677cd694c78ed3c0d7c3b
Muhammad Zunair <muhammad.zunair@xprolabs.com>
2021-09-08 19:17:20 +0500

    download pdf

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,5 +2,11 @@ class EmployeesController < ApplicationController
   before_action :authenticate_user!
   def index
     @tasks = current_user.tasks.all
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "status report", template: "employees/index.html.erb"   # Excluding ".pdf" extension.
+      end
+    end
   end
 end
