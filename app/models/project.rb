cefba33b19e2134f0c8ecd8e2c11175c90aceb66
Muhammad Zunair <muhammad.zunair@xprolabs.com>
2021-09-08 15:51:12 +0500

    status management

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,12 @@
 class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
   belongs_to :user
+
+  validates :status, inclusion: { in: ['pending', 'in-progress', 'complete'] }
+
+  STATUS_OPTIONS = [
+    ['Pending', 'pending'],
+    ['In progress', 'in-progress'],
+    ['Complete', 'complete']
+  ]
 end
