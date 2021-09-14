eb23be71aff2d1ebb6caf0a05b9576d043d9822f
Muhammad Zunair <muhammad.zunair@xprolabs.com>
2021-09-14 13:51:02 +0500

    admin section: view users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-     if @user.save
-       redirect_to root_path
-     end
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to root_path, notice: "User was successfully created." }
+      end
+    end
   end
 
   private
