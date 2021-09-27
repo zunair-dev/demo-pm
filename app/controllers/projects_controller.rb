dabec54f9e9c9f9a5c2ee50a7c1fee39bffb1598
Muhammad Zunair <muhammad.zunair@xprolabs.com>
2021-09-27 09:21:08 +0500

    all projects view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,7 +69,7 @@ class ProjectsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = current_user.projects.find(params[:id])
+      @project = Project.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
